38db25e2ecad8e60363181ec18ec1a2816835633
C-Annunziato <68083360+C-Annunziato@users.noreply.github.com>
2023-01-09 21:01:51 -0500

    number formatting fixed app functional

--- app/src/main/java/com/example/tiptimecompose/MainActivity.kt
+++ app/src/main/java/com/example/tiptimecompose/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.tiptimecompose
 
 import android.os.Bundle
-import com.example.tiptimecompose.ui.theme.TipTimeComposeTheme
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -13,11 +12,14 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlin.math.cos
+import java.util.Locale
+
+const val TAG = "main"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,41 +47,55 @@ fun TipTimeScreen(modifier: Modifier = Modifier) {
         )
         Spacer(modifier = Modifier.height(16.dp))
         EditNumberfield(value = costOfService, onValueChange = { costOfService = it })
+        Spacer(modifier = Modifier.height(16.dp))
+
+        AmountsDisplayed(bill = costOfService.toDoubleOrNull() ?: 0.0)
     }
 }
 
 @Composable
 fun EditNumberfield(value: String, onValueChange: (String) -> Unit) {
 
-    TextField(
-        value = value,
+    TextField(value = value,
         onValueChange = onValueChange,
         singleLine = true,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         label = {
             Text(
                 stringResource(
-                    id = R.string.cost_of_service
-                ),
-                modifier = Modifier.fillMaxWidth()
+                    id = R.string.cost_of_service)
             )
-        }
-    )
+        })
+
+
 }
 
-private fun calculateTip(amount: Double, tipPercent: Double){
-    var totalTip = (amount * (tipPercent/100)) + amount
-}
-
-
-
-@Preview(showBackground = true)
 @Composable
-fun Preview() {
+fun AmountsDisplayed(bill: Double) {
 
+    val tip = calculateTip(bill)
+    val total = "%.2f".format(Locale.US,tip.toDouble() + bill)
+
+    Column() {
+        Text(
+            text = "Tip:                      $tip",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center)
+        Text(
+            text = "Total amount:    $total",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
+private fun calculateTip(bill: Double, tipPercent: Double = 15.0): String {
 
+    return "%.2f".format(Locale.US, bill * (tipPercent / 100))
+
+}
 
 
 
